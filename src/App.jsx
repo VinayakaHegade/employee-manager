3146b5b02bbdc9483db8d14c881e1fbcff3e2829
Vinayak <myselfvinayakhegde@gmail.com>
2024-03-04 01:25:52 +0530

    feat: add react-router for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import styles from "./App.module.css";
+import Login from "./pages/Login/Login";
 
 function App() {
-
-
   return (
-    <div>
-      <h1>Moptro</h1>
+    <div className={styles.app}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
